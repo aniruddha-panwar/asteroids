53e82ea469115887cf7e69b7371c64545e5e6145
Ani Panwar <15876706+aniruddha-panwar@users.noreply.github.com>
2024-11-24 15:07:12 -0800

    First pass game loop with black screen

--- main.py
+++ main.py
@@ -2,9 +2,20 @@ import pygame
 from constants.constants import SCREEN
 
 def main():
+    numpass, numfail = pygame.init()
+    # game screen
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN.SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN.SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN.SCREEN_WIDTH, SCREEN.SCREEN_HEIGHT))
+    
+    # game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
 
 if __name__ == '__main__':
     main()
