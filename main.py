6660d8d041988637d5e58709e22e34a65a485608
Ani Panwar <15876706+aniruddha-panwar@users.noreply.github.com>
2024-11-24 13:12:06 -0800

    Creating a first pass for constants module Updating .gitignore to ignore __pycache__

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pygame
+from constants.constants import SCREEN
 
 def main():
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN.SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN.SCREEN_HEIGHT}")
 
 if __name__ == '__main__':
     main()
