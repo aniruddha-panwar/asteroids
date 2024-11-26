fa81a0cb6e2340f471f7e57b6582d18c094f1384
Ani Panwar <15876706+aniruddha-panwar@users.noreply.github.com>
2024-11-25 21:01:08 -0800

    Split them roids

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 from circleshape import CircleShape
 import pygame
+from constants.constants import ASTEROIDSTATS
+from random import uniform
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
@@ -15,3 +17,20 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += (self.velocity*dt)
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROIDSTATS.ASTEROID_MIN_RADIUS:
+            return
+        # Spawn 2 new children asteroids
+        # children will split and move between 20-50 degrees from original path
+        split_children_angle = uniform(20,50)
+        child_vector1 = self.velocity.rotate(split_children_angle)
+        child_vector2 = self.velocity.rotate(-split_children_angle)
+        child_radius = self.radius - ASTEROIDSTATS.ASTEROID_MIN_RADIUS
+        child_asteroid1 = Asteroid(self.position.x, self.position.y, child_radius)
+        child_asteroid1.velocity = child_vector1 * 1.2
+        child_asteroid2 = Asteroid(self.position.x, self.position.y, child_radius)
+        child_asteroid2.velocity = child_vector2 * 1.2
+
+
